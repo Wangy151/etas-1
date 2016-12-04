818573e4ca1e0bb7f36ea8e2eea57e92dbb4bd5b
Wy <398830945@qq.com>
2016-12-04 18:35:53 +0800

    修改密码找回页面

--- src/main/resources/static/js/findPasswordFormValidate.js
+++ src/main/resources/static/js/findPasswordFormValidate.js
@@ -10,10 +10,6 @@ function findPasswordFormValidate() {
         rules: {
             userId: {
                 required: true,
-                remote: { //远程验证用户名是否已经存在,若存在false，否则true
-                    url: "/register/checkUserIdRegister",//异步验证
-                    type: "get",
-                },
             },
             validateCode: {
                 required: true,
@@ -21,19 +17,17 @@ function findPasswordFormValidate() {
         },
 
         messages: {
-            // userId: {
-            //     required: "学号/教工号不能为空",
-            //     remote: "用户不存在",
-            // },
+            userId: {
+                required: "学号/教工号不能为空",
+            },
             validateCode: {
                 required: "验证码不为空",
             },
         },
         errorPlacement: function (error, element) {
-            // if (element.is("#userId"))
-            //     error.appendTo($("#v_idnumber"));
-            // else
-            if (element.is("#validateCode"))
+            if (element.is("#userId"))
+                error.appendTo($("#v_idnumber"));
+            else if (element.is("#validateCode"))
                 error.appendTo($("#v_validateCode"));
             else ;
         }
