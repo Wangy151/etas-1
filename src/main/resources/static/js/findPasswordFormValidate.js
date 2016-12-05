7c1c6c8ed6991d6cf74eed7e1be174f364594774
Wy <398830945@qq.com>
2016-12-05 23:15:22 +0800

    找回密码页面 1.0版本，增加找回成功后的提示

--- src/main/resources/static/js/findPasswordFormValidate.js
+++ src/main/resources/static/js/findPasswordFormValidate.js
@@ -24,12 +24,18 @@ function findPasswordFormValidate() {
                 required: "验证码不为空",
             },
         },
+        // errorPlacement: function (error, element) {
+        //     if (element.is("#userId"))
+        //         error.appendTo($("#v_validateCode"));
+        //     else if (element.is("#validateCode"))
+        //         error.appendTo($("#v_validateCode"));
+        //     else ;
+        // }
         errorPlacement: function (error, element) {
             if (element.is("#userId"))
-                error.appendTo($("#v_idnumber"));
+                $("#v_validateCode").html(error);
             else if (element.is("#validateCode"))
-                error.appendTo($("#v_validateCode"));
-            else ;
+                $("#v_validateCode").html(error);
         }
     })
 }
