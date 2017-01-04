54f450aed901c8f8805730cc31fce64ba1bc7393
xiaolei03 <xiaolei03@baidu.com>
2017-01-04 16:04:44 +0800

    硕士论文推荐表: 保存功能

---
+++ src/main/java/cn/edu/hust/model/MasterThesisApply.java
@@ -0,0 +1,459 @@
+package cn.edu.hust.model;
+
+/**
+ * Created by xiaolei03 on 17/1/4.
+ */
+public class MasterThesisApply {
+    private String zzxh;
+    private String zzxm;
+    private String xb;
+    private String csny;
+    private String mz;
+    private String lwtm;
+    private String lwywtm;
+    private String rxny;
+    private String dbrq ;
+    private String hdxwrq;
+    private String yjxkdm;
+    private String yjxkmc;
+    private String ejxkdm;
+    private String ejxkmc;
+    private String lwsjdyjfx;
+
+
+    private int dyzz;
+    private int dezz;
+    private int sci;
+    private int ei;
+    private int ssci;
+    private int istp;
+    private int zls;
+    private String cgjx;
+
+    private String gdxwfs;
+    private String bkjdxx;
+    private String gdssxwdw;
+    private String zzdw;
+    private String zzdz;
+    private String zzyb;
+    private String zzdh;
+    private String zc;
+    private String zw;
+    private String zdjsxm;
+    private String zdjsyjfx;
+
+    private String fbxslw;
+    private String cbzz;
+    private String hjxm;
+    private String lwdzycxd;
+    private String dwtjyy;
+    private String tbrq;
+
+    private String dwdm;
+    private String dwmc;
+    private String studentType;
+
+    private String part;
+
+    public String getZzxh() {
+        return zzxh;
+    }
+
+    public void setZzxh(String zzxh) {
+        this.zzxh = zzxh;
+    }
+
+    public String getZzxm() {
+        return zzxm;
+    }
+
+    public void setZzxm(String zzxm) {
+        this.zzxm = zzxm;
+    }
+
+    public String getXb() {
+        return xb;
+    }
+
+    public void setXb(String xb) {
+        this.xb = xb;
+    }
+
+    public String getCsny() {
+        return csny;
+    }
+
+    public void setCsny(String csny) {
+        this.csny = csny;
+    }
+
+    public String getMz() {
+        return mz;
+    }
+
+    public void setMz(String mz) {
+        this.mz = mz;
+    }
+
+    public String getLwtm() {
+        return lwtm;
+    }
+
+    public void setLwtm(String lwtm) {
+        this.lwtm = lwtm;
+    }
+
+    public String getLwywtm() {
+        return lwywtm;
+    }
+
+    public void setLwywtm(String lwywtm) {
+        this.lwywtm = lwywtm;
+    }
+
+    public String getRxny() {
+        return rxny;
+    }
+
+    public void setRxny(String rxny) {
+        this.rxny = rxny;
+    }
+
+    public String getDbrq() {
+        return dbrq;
+    }
+
+    public void setDbrq(String dbrq) {
+        this.dbrq = dbrq;
+    }
+
+    public String getHdxwrq() {
+        return hdxwrq;
+    }
+
+    public void setHdxwrq(String hdxwrq) {
+        this.hdxwrq = hdxwrq;
+    }
+
+    public String getYjxkdm() {
+        return yjxkdm;
+    }
+
+    public void setYjxkdm(String yjxkdm) {
+        this.yjxkdm = yjxkdm;
+    }
+
+    public String getYjxkmc() {
+        return yjxkmc;
+    }
+
+    public void setYjxkmc(String yjxkmc) {
+        this.yjxkmc = yjxkmc;
+    }
+
+    public String getEjxkdm() {
+        return ejxkdm;
+    }
+
+    public void setEjxkdm(String ejxkdm) {
+        this.ejxkdm = ejxkdm;
+    }
+
+    public String getEjxkmc() {
+        return ejxkmc;
+    }
+
+    public void setEjxkmc(String ejxkmc) {
+        this.ejxkmc = ejxkmc;
+    }
+
+    public String getLwsjdyjfx() {
+        return lwsjdyjfx;
+    }
+
+    public void setLwsjdyjfx(String lwsjdyjfx) {
+        this.lwsjdyjfx = lwsjdyjfx;
+    }
+
+    public int getDyzz() {
+        return dyzz;
+    }
+
+    public void setDyzz(int dyzz) {
+        this.dyzz = dyzz;
+    }
+
+    public int getDezz() {
+        return dezz;
+    }
+
+    public void setDezz(int dezz) {
+        this.dezz = dezz;
+    }
+
+    public int getSci() {
+        return sci;
+    }
+
+    public void setSci(int sci) {
+        this.sci = sci;
+    }
+
+    public int getEi() {
+        return ei;
+    }
+
+    public void setEi(int ei) {
+        this.ei = ei;
+    }
+
+    public int getSsci() {
+        return ssci;
+    }
+
+    public void setSsci(int ssci) {
+        this.ssci = ssci;
+    }
+
+    public int getIstp() {
+        return istp;
+    }
+
+    public void setIstp(int istp) {
+        this.istp = istp;
+    }
+
+    public int getZls() {
+        return zls;
+    }
+
+    public void setZls(int zls) {
+        this.zls = zls;
+    }
+
+    public String getCgjx() {
+        return cgjx;
+    }
+
+    public void setCgjx(String cgjx) {
+        this.cgjx = cgjx;
+    }
+
+    public String getGdxwfs() {
+        return gdxwfs;
+    }
+
+    public void setGdxwfs(String gdxwfs) {
+        this.gdxwfs = gdxwfs;
+    }
+
+    public String getBkjdxx() {
+        return bkjdxx;
+    }
+
+    public void setBkjdxx(String bkjdxx) {
+        this.bkjdxx = bkjdxx;
+    }
+
+    public String getGdssxwdw() {
+        return gdssxwdw;
+    }
+
+    public void setGdssxwdw(String gdssxwdw) {
+        this.gdssxwdw = gdssxwdw;
+    }
+
+    public String getZzdw() {
+        return zzdw;
+    }
+
+    public void setZzdw(String zzdw) {
+        this.zzdw = zzdw;
+    }
+
+    public String getZzdz() {
+        return zzdz;
+    }
+
+    public void setZzdz(String zzdz) {
+        this.zzdz = zzdz;
+    }
+
+    public String getZzyb() {
+        return zzyb;
+    }
+
+    public void setZzyb(String zzyb) {
+        this.zzyb = zzyb;
+    }
+
+    public String getZzdh() {
+        return zzdh;
+    }
+
+    public void setZzdh(String zzdh) {
+        this.zzdh = zzdh;
+    }
+
+    public String getZc() {
+        return zc;
+    }
+
+    public void setZc(String zc) {
+        this.zc = zc;
+    }
+
+    public String getZw() {
+        return zw;
+    }
+
+    public void setZw(String zw) {
+        this.zw = zw;
+    }
+
+    public String getZdjsxm() {
+        return zdjsxm;
+    }
+
+    public void setZdjsxm(String zdjsxm) {
+        this.zdjsxm = zdjsxm;
+    }
+
+    public String getZdjsyjfx() {
+        return zdjsyjfx;
+    }
+
+    public void setZdjsyjfx(String zdjsyjfx) {
+        this.zdjsyjfx = zdjsyjfx;
+    }
+
+    public String getFbxslw() {
+        return fbxslw;
+    }
+
+    public void setFbxslw(String fbxslw) {
+        this.fbxslw = fbxslw;
+    }
+
+    public String getCbzz() {
+        return cbzz;
+    }
+
+    public void setCbzz(String cbzz) {
+        this.cbzz = cbzz;
+    }
+
+    public String getHjxm() {
+        return hjxm;
+    }
+
+    public void setHjxm(String hjxm) {
+        this.hjxm = hjxm;
+    }
+
+    public String getLwdzycxd() {
+        return lwdzycxd;
+    }
+
+    public void setLwdzycxd(String lwdzycxd) {
+        this.lwdzycxd = lwdzycxd;
+    }
+
+    public String getDwtjyy() {
+        return dwtjyy;
+    }
+
+    public void setDwtjyy(String dwtjyy) {
+        this.dwtjyy = dwtjyy;
+    }
+
+    public String getTbrq() {
+        return tbrq;
+    }
+
+    public void setTbrq(String tbrq) {
+        this.tbrq = tbrq;
+    }
+
+    public String getDwdm() {
+        return dwdm;
+    }
+
+    public void setDwdm(String dwdm) {
+        this.dwdm = dwdm;
+    }
+
+    public String getDwmc() {
+        return dwmc;
+    }
+
+    public void setDwmc(String dwmc) {
+        this.dwmc = dwmc;
+    }
+
+    public String getStudentType() {
+        return studentType;
+    }
+
+    public void setStudentType(String studentType) {
+        this.studentType = studentType;
+    }
+
+    public String getPart() {
+        return part;
+    }
+
+    public void setPart(String part) {
+        this.part = part;
+    }
+
+    @Override
+    public String toString() {
+        return "MasterThesisApply{" +
+                "zzxh='" + zzxh + '\'' +
+                ", zzxm='" + zzxm + '\'' +
+                ", xb='" + xb + '\'' +
+                ", csny='" + csny + '\'' +
+                ", mz='" + mz + '\'' +
+                ", lwtm='" + lwtm + '\'' +
+                ", lwywtm='" + lwywtm + '\'' +
+                ", rxny='" + rxny + '\'' +
+                ", dbrq='" + dbrq + '\'' +
+                ", hdxwrq='" + hdxwrq + '\'' +
+                ", yjxkdm='" + yjxkdm + '\'' +
+                ", yjxkmc='" + yjxkmc + '\'' +
+                ", ejxkdm='" + ejxkdm + '\'' +
+                ", ejxkmc='" + ejxkmc + '\'' +
+                ", lwsjdyjfx='" + lwsjdyjfx + '\'' +
+                ", dyzz=" + dyzz +
+                ", dezz=" + dezz +
+                ", sci=" + sci +
+                ", ei=" + ei +
+                ", ssci=" + ssci +
+                ", istp=" + istp +
+                ", zls=" + zls +
+                ", cgjx='" + cgjx + '\'' +
+                ", gdxwfs='" + gdxwfs + '\'' +
+                ", bkjdxx='" + bkjdxx + '\'' +
+                ", gdssxwdw='" + gdssxwdw + '\'' +
+                ", zzdw='" + zzdw + '\'' +
+                ", zzdz='" + zzdz + '\'' +
+                ", zzyb='" + zzyb + '\'' +
+                ", zzdh='" + zzdh + '\'' +
+                ", zc='" + zc + '\'' +
+                ", zw='" + zw + '\'' +
+                ", zdjsxm='" + zdjsxm + '\'' +
+                ", zdjsyjfx='" + zdjsyjfx + '\'' +
+                ", fbxslw='" + fbxslw + '\'' +
+                ", cbzz='" + cbzz + '\'' +
+                ", hjxm='" + hjxm + '\'' +
+                ", lwdzycxd='" + lwdzycxd + '\'' +
+                ", dwtjyy='" + dwtjyy + '\'' +
+                ", tbrq='" + tbrq + '\'' +
+                ", dwdm='" + dwdm + '\'' +
+                ", dwmc='" + dwmc + '\'' +
+                ", studentType='" + studentType + '\'' +
+                ", part='" + part + '\'' +
+                '}';
+    }
+}
