6753dd5b318c724bc0e5070a43bcea552057b594
meihao <meihao_private@126.com>
2017-01-05 23:57:51 +0800

    学生模块申请状态js代码编写

--- src/main/resources/static/js/general_help.js
+++ src/main/resources/static/js/general_help.js
@@ -2,8 +2,20 @@
  * Created by Administrator on 2016/12/25.
  */
 
-function model_tip_show(model_id,tip_id,content){
+function model_tip_show(model_id,tip_id,content,close_func){
     $("#"+tip_id).html(""+content);
 
     $("#"+model_id).modal('show');
+
+    $("#model_tip_close_btn").click(close_func);
+}
+
+//model_ok_show("model_ok","model_ok_content","确认是否提交申请","model_ok_btn",success_func)
+function model_ok_show(model_id,content_id,content,ok_btn_id,success_func){
+    $("#"+content_id).html(""+content);
+
+    $("#"+model_id).modal('show');
+
+    $("#"+ok_btn_id).click(success_func);
+
 }
--- src/main/resources/static/js/s_query_thesis_status.js
+++ src/main/resources/static/js/s_query_thesis_status.js
@@ -3,6 +3,9 @@
  */
     $(document).ready(function () {
         applyThesis();
+        checkDetail();
+        modifyApply();
+        deleteApply();
     });
 
 function applyThesis(){
@@ -14,39 +17,174 @@ function applyThesis(){
         }else if(length<=0){//未选中
             model_tip_show('model_tip','model_tip_content','请选择要提交的申请');
         }else{ //选中了1个
-            $.ajax({
-                type: "POST",
-                url: "/home/student/thesis/manage/submit",
-                contentType: "application/json",
-                data: JSON.stringify({
+            //1.确定是否提交申请
+            model_ok_show("model_ok","model_ok_content","确认是否提交申请","model_ok_btn",applyThesis1);
 
-                }),
-
-                beforeSend: function(XMLHttpRequest){
-                },
-
-                success: function(data){
-                    // 200 成功    300 重复申请  500 失败
-                    var status = data.code;
-                    var msg = data.msg;
-                    if(status == "200")  //200 成功
-                        model_tip_show('model_tip','model_tip_content','提交申请成功');
-                    else if(status == "300")  //300 重复申请
-                        model_tip_show('model_tip','model_tip_content','您已提交申请, 请关注申请状态');
-                    else if(status == "500")  //服务器繁忙
-                        model_tip_show('model_tip','model_tip_content','服务器繁忙，请稍后再试');
-                    else
-                        var empty = "";
-                },
-
-                error: function(XMLHttpRequest, textStatus) {
-                },
-
-                complete: function(XMLHttpRequest, textStatus){
-                }
-
-            });
         } //else
 
     }) //click
+}
+
+function applyThesis1(){
+    $("#model_ok").modal("hide");
+    $.ajax({
+        type: "POST",
+        url: "/home/student/thesis/manage/submit",
+        contentType: "application/json",
+        data: JSON.stringify({
+
+        }),
+
+        beforeSend: function(XMLHttpRequest){
+        },
+
+        success: function(data){
+            // 200 成功    300 重复申请  500 失败
+            var status = data.code;
+            var msg = data.msg;
+            if(status == "200"){//200 成功
+                model_tip_show('model_tip','model_tip_content','提交申请成功',refreshToStudentQueryStatus);
+            }
+            else if(status == "300")  //300 重复申请
+                model_tip_show('model_tip','model_tip_content','您已提交申请, 请关注申请状态');
+            else if(status == "500")  //服务器繁忙
+                model_tip_show('model_tip','model_tip_content','服务器繁忙，请稍后再试');
+            else
+                var empty = "";
+        },
+
+        error: function(XMLHttpRequest, textStatus) {
+        },
+
+        complete: function(XMLHttpRequest, textStatus){
+        }
+
+    });
+}
+
+
+
+
+//删除申请按钮事件
+function deleteApply(){
+    //delete_btn
+    $("#delete_btn").click(function(){
+        //确定是否删除
+        model_ok_show("model_ok","model_ok_content","确认是否要删除申请","model_ok_btn",deleteApply1);
+
+    });//click
+}
+
+function deleteApply1(){
+    $("#model_ok").modal("hide");
+    //1.删除该申请
+    // url: /home/student/thesis/manage/delete
+    //删除论文申请    200 成功    300 学院教务员已审核通过, 不能删除     500 失败
+    $.ajax({
+        type: "POST",
+        url: "/home/student/thesis/manage/delete",
+        contentType: "application/json",
+        data: JSON.stringify({
+
+        }),
+
+        beforeSend: function(XMLHttpRequest){
+        },
+
+        success: function(data){
+            // 200 成功    300 学院教务员已审核通过, 不能删除     500 失败
+            var status = data.code;
+            var msg = data.msg;
+            if(status == "200"){//200 成功
+                model_tip_show('model_tip','model_tip_content','删除申请成功',refreshToStudentQueryStatus);
+            }
+            else if(status == "300")  //300 学院教务员已审核通过, 不能删除
+                model_tip_show('model_tip','model_tip_content','学院教务员已审核通过, 不能删除');
+            else if(status == "500")  //服务器繁忙
+                model_tip_show('model_tip','model_tip_content','服务器繁忙，请稍后再试');
+            else
+                var empty = "";
+        },
+        error: function(XMLHttpRequest, textStatus) {
+
+        },
+        complete: function(XMLHttpRequest, textStatus){
+        }
+    }); // end ajax
+
+    //2.页面重新刷新
+    refreshToStudentQueryStatus();
+}
+
+
+
+//查看详情按钮事件
+function checkDetail(){
+    //checke_btn
+    $("#checke_btn").click(function(){
+        refreshToStudentApplyThesis();
+    });//click
+}
+
+//修改申请按钮事件
+function modifyApply(){
+    //modify_btn
+    $("#modify_btn").click(function(){
+        refreshToStudentApplyThesis();
+    });//click
+}
+
+
+function refreshToStudentQueryStatus(){
+    $.ajax({
+        type: "POST",
+        url: "/home/student/thesis/manage/index",
+        contentType: "application/json",
+        data: JSON.stringify({
+
+        }),
+
+        beforeSend: function(XMLHttpRequest){
+        },
+
+        success: function(data){
+            $("#home_right_wrap").html(data);
+        },
+        error: function(XMLHttpRequest, textStatus) {
+            if (XMLHttpRequest.status == 401) {
+                $("#home_right_wrap").html("您没有访问权限 ~");
+            } else {
+                $("#home_right_wrap").html("服务器繁忙, 请稍后再试 ~");
+            }
+        },
+        complete: function(XMLHttpRequest, textStatus){
+        }
+    }); // end ajax
+}
+
+function refreshToStudentApplyThesis(){
+    $.ajax({
+        type: "POST",
+        url: "/home/student/apply/index",
+        contentType: "application/json",
+        data: JSON.stringify({
+
+        }),
+
+        beforeSend: function(XMLHttpRequest){
+        },
+
+        success: function(data){
+            $(".mid_body").html(data);
+        },
+        error: function(XMLHttpRequest, textStatus) {
+            if (XMLHttpRequest.status == 401) {
+                $("#home_right_wrap").html("您没有访问权限 ~");
+            } else {
+                $("#home_right_wrap").html("服务器繁忙, 请稍后再试 ~");
+            }
+        },
+        complete: function(XMLHttpRequest, textStatus){
+        }
+    }); // end ajax
 }
