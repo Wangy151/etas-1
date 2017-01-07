7b4321afaf9917e13b7f76414464d89aef69f1c7
meihao <meihao_private@126.com>
2017-01-07 14:57:51 +0800

    学院上报状态js代码编写

--- src/main/resources/static/js/teacher_report_thesis_divd_table.js
+++ src/main/resources/static/js/teacher_report_thesis_divd_table.js
@@ -32,6 +32,7 @@ function no_select_all(){
 function report(){
     //report_btn
     $("#report_btn").click(function () {
+        //检查是否选中
         var checked = $("input[name='checkboxStatus']:checked");
         var checked_length = checked.length;
         var xh_array = new Array();
@@ -39,6 +40,14 @@ function report(){
             model_tip_show('model_tip1','model_tip_content1','未选择申请!');
             return;
         }
+        //如果选中了
+
+        //1.检查选中的申请的状态是否有不可上报状态
+        if(checkBeforeReport()==false){
+            model_tip_show('model_tip1','model_tip_content1','选中的记录中有不是“待学院上报”状态');
+            return;
+        }
+        //2.如果选中的是正确，准备将数据发往服务器
         checked.each(function(){ //将选中的学号放到xh_array数组中
             var value = $(this).parent().next().next().html();
             xh_array.push(value);
@@ -75,9 +84,11 @@ function report(){
     }) //click
 } //function
 
+
 function cancel_report(){
     //cancel_report_btn
     $("#cancel_report_btn").click(function () {
+        //1.检查是否有选中
         var checked = $("input[name='checkboxStatus']:checked");
         var checked_length = checked.length;
         var xh_array = new Array();
@@ -85,6 +96,13 @@ function cancel_report(){
             model_tip_show('model_tip1','model_tip_content1','未选择申请!');
             return;
         }
+        //2.检查选中的状态是否正确
+        if(checkBeforeCancelReport()==false){
+            model_tip_show('model_tip1','model_tip_content1','选中的记录中有不是“待学校审核状态”');
+            return;
+        }
+
+        //3.组装数据发给后台服务器处理
         checked.each(function(){ //将选中的学号放到xh_array数组中
             var value = $(this).parent().next().html();
             xh_array.push(value);
@@ -119,4 +137,37 @@ function cancel_report(){
         }); //ajax
 
     }) //click
+}
+
+function checkBeforeReport(){
+    var _checked = $("input[name='checkboxStatus']:checked");
+    var _status =  _checked.parent().parent().children("td.apply_status");
+    var inspectV = 1;
+    _status.each(function(){
+        if($(this).text() !='待学院上报'){
+            inspectV = 0;
+            return;
+        }
+    });
+    if(inspectV==1)
+        return true;
+    else
+        return false;
+}
+
+
+function checkBeforeCancelReport(){
+    var _checked = $("input[name='checkboxStatus']:checked");
+    var _status =  _checked.parent().parent().children("td.apply_status");
+    var inspectV = 1;
+    _status.each(function(){
+        if($(this).text() !='待学校审核'){
+            inspectV = 0;
+            return;
+        }
+    });
+    if(inspectV==1)
+        return true;
+    else
+        return false;
 }
