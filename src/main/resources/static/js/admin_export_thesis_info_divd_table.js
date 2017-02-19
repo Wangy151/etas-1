4d8ec935f5240fc4efee259cbcc0eb5e38c52dd7
jason <meihao_private@126.com>
2017-02-19 15:48:51 +0800

    小修改

--- src/main/resources/static/js/admin_export_thesis_info_divd_table.js
+++ src/main/resources/static/js/admin_export_thesis_info_divd_table.js
@@ -58,39 +58,25 @@ function exportExeclFile1(){
     checked.each(function(){ //将选中的学号放到xh_array数组中
         var value = $(this).parent().parent().children("td.userId").text();
         xh_array.push(value);
-    })
+    });
+    // alert("组装form开始");
     //2.开始向后台发送数据
-    $.ajax({
-        type: "POST",
-        url: exportExeclFileUrl,
-        contentType: "application/json",
-        data: JSON.stringify({
-            "userIds":xh_array,
-        }),
+    var form=$("<form>");//定义一个form表单
+    form.attr("style","display:none");
+    form.attr("target","_self");
+    form.attr("method","post");
+    form.attr("action",exportExeclFileUrl);
+    var input1=$("<input>");
+    input1.attr("type","hidden");
+    input1.attr("name","userIds");
+    input1.attr("value",xh_array);
+    $(".table_body").append(form);//将表单放置在web中
+    form.append(input1);
 
-        beforeSend: function(XMLHttpRequest){
-        },
+    var a = form;
+    // alert("组装form结束，准备提交");
 
-        success: function(data){
-            // 200 成功    300 重复申请  500 失败
-            closeModal("model_ok");
-            var status = data.code;
-            var msg = data.msg;
-            if(status == "200")  //200 成功
-                model_tip_show('model_tip1','model_tip_content1','取消审核成功',refreshToAdminSearchThesisPage);
-            else if(status == "500")  //服务器繁忙
-                model_tip_show('model_tip','model_tip_content','服务器繁忙，请稍后再试');
-            else
-                model_tip_show('model_tip','model_tip_content','服务器开小差了, 请稍后再试');
-        },
-
-        error: function(XMLHttpRequest, textStatus) {
-        },
-
-        complete: function(XMLHttpRequest, textStatus){
-        }
-
-    }); //ajax
+    form.submit();//表单提交
 
 }
 
