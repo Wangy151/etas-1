43008c57f368a833d5d98aee0ba3546c23c2698d
ray <ray_here@qq.com>
2017-03-19 00:29:41 +0800

    update

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ MAX_PERM_MEMORY=512M
 
 java -Dspring.config.location=$SPRING_CONFIG_FILE -Dfile.encoding=UTF-8 \
 -Xmx$MAX_MEMORY -XX:MaxPermSize=$MAX_PERM_MEMORY -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps \
--Xloggc:gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=20M -jar $APPLICATION
+-Xloggc:gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=20M -jar $APPLICATION > /dev/null 2>&1 &
