3194343e3caaaa79a6486610f0730cbfae2c4e83
ray <ray_here@qq.com>
2017-03-18 22:30:36 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /home/ray
+
+rm -rf workspace
+mkdir workspace
+
+# cp target/etas-1.0.jar output/etas/bin/etas.jar
+# cp start.sh output/etas/bin
+# cp stop.sh output/etas/bin
+# cp target/classes/application.properties output/etas/conf
